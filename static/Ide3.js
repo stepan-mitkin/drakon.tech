23becc4a3268ef73fb6f2a4e61f55f6abbeeb747
stepan-mitkin <stipan.mitkin@gmail.com>
2021-03-15 08:43:44 +0100

    Added JS new/old labels

--- static/Ide3.js
+++ static/Ide3.js
@@ -2484,10 +2484,10 @@ function makeImagePath(image) {
 function makeLanguageList(div, node, widget) {
     var select = make(div, "select")
     select.id = "language_list"
+    addOption(select, "LANG_JS2", translate("LANG_JS2"))
     addOption(select, "LANG_JS", translate("LANG_JS"))
     addOption(select, "LANG_HUMAN", translate("LANG_HUMAN"))
-    addOption(select, "LANG_JS2", translate("LANG_JS2"))
-    select.value = globs.props.language || "LANG_JS"
+    select.value = globs.props.language || "LANG_JS2"
     select.addEventListener("change", onLanguageChange)
 }
 
